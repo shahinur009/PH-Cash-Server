9299ca54952b8529c5119022cc77b7fc261aad6c
BP Devs <bpdevs1037@gmail.com>
2025-02-26 23:25:07 +0600

    verified-token

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ph-kash-client.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://ph-kash-client.vercel.app",
+      "https://ph-cash-server.vercel.app",
+    ],
     credentials: true,
   })
 );
